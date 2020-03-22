d3334ab426afd5338db5603f22d4a522f3e4ff88
Catharina <catharina.sorum@outlook.com>
2020-03-22 12:52:32 +0100

    added code

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,70 @@
-<page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
 <script>
-    let message = "Blank Svelte Native App"
+import { onMount } from "svelte"
+import { showModal } from "svelte-native"
+import Article from "./modals/Article.svelte"
+
+const api = "5601425ba11d4e97bc73a41da921ee79"
+const subject = "travel"
+
+const getData = () => {
+    const url = `https://newsapi.org/v2/everything?q=${subject}&language=en&apiKey=${api}`
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            articles = json.articles
+        })
+        .catch(error => console.log(error))
+}
+
+let articles = []
+
+onMount(() => {
+    getData()
+})
+
+const showArticle = async (article) => {
+    await showModal({
+        page: Article,
+        props: {
+            article:article
+        }
+    })
+}
 </script>
 
+<page>
+    <actionBar title="World Wide Travel News" flat="true" />
+    <stackLayout>
+
+        <scrollView>
+            <stackLayout class="articles">
+                {#each articles as article}
+                    <stackLayout
+                        on:tap={ () => showArticle(article)}
+                        class="article">
+                        <image class='img-rounded' src='{article.urlToImage}' alt='cover' stretch='aspectFill' />
+                        <label textWrap="{true}" class="h2 text-center" text="{article.title}" />
+                    </stackLayout>
+                {:else}
+                    <label text="Loading" />
+                {/each}
+            </stackLayout>
+
+        </scrollView>
+
+    </stackLayout>
+</page>
+
+
 <style>
-    .info .fas {
-        color: #3A53FF;
+    .articles{
+        width: 100%;
     }
-    .info {
-        font-size: 20;
+
+    .article{
+        min-height: 100;
+        padding: 24;
+        margin: 24;
+        background-color: #eee;
     }
 </style>
---
+++ app/modals/Article.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { closeModal } from "svelte-native"
+    export let article
+</script>
+
+<frame>
+    <page>
+        <scrollView>
+            <stackLayout class="article">
+                <label class="h1" text="{article.title}" textWrap="{true}" />
+                <label class="body" text="written by {article.author}" textWrap="{true}" />
+                <textView class="primary space" text="{article.description}" textWrap="{true}" />
+                <!--<button on:tap={goToURL} text="Read more"/> Hadde plan om å ha en "Read More" button for å kunne lese artikkelen i sin helhet.-->
+                <image
+                    class="img rounded"
+                    src="{article.urlToImage}"
+                    alt="article cover image"
+                    stretch="fill" />
+                
+                <button on:tap={ () => closeModal() } text="close" />
+            </stackLayout>
+        </scrollView>
+    </page>
+</frame>
+
+<style>
+    .article {
+        padding: 44;
+    }
+    .space {
+        margin: 30 0;
+        line-height: 5;
+    }
+</style>
